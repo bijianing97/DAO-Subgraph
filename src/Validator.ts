a8d18064e8578dc1c0cfee169f1e54a8eba77171
sspin <826015751@qq.com>
2022-01-24 18:01:51 +0800

    chore: add validator active property

--- src/Validator.ts
+++ src/Validator.ts
@@ -2,14 +2,10 @@ import { Address, BigInt, ethereum } from '@graphprotocol/graph-ts'
 import { Validator, ValidatorInfo } from './types/schema'
 import { stakemanager } from './utils/helper'
 
-function createValidatorInfo(address: Address, blocknumber: BigInt): string {
+function createValidatorInfo(address: Address, blocknumber: BigInt, active: boolean): string {
   const validator = stakemanager.validators(address)
   const id = `${address.toHex()}-${blocknumber.toHex()}`
-  let ValidatorInfoInstance = ValidatorInfo.load(id)
-  if (ValidatorInfoInstance) {
-    return id
-  }
-  ValidatorInfoInstance = new ValidatorInfo(id)
+  const ValidatorInfoInstance = new ValidatorInfo(id)
   ValidatorInfoInstance.address = address
   ValidatorInfoInstance.commissionRate = validator.value2
   ValidatorInfoInstance.commissionAddress = validator.value1
@@ -26,16 +22,32 @@ export function handleValidatorBlock(block: ethereum.Block): void {
   const id = block.number.toString()
   const ValidatorInstance = new Validator(id)
   const indexedValidatorsLength = stakemanager.indexedValidatorsLength()
+  const activeAddress: Address[] = []
+  const indexedAddress: Address[] = []
+  for (let i = BigInt.fromU32(0); i.lt(activeValidatorsLengthResult.value); i = i.plus(BigInt.fromU32(1))) {
+    activeAddress.push(stakemanager.activeValidators(i).value0)
+  }
+  for (let i = BigInt.fromU32(0); i.lt(indexedValidatorsLength); i = i.plus(BigInt.fromU32(1))) {
+    indexedAddress.push(stakemanager.indexedValidatorsByIndex(i))
+  }
+
+  for (let i = indexedAddress.length - 1; i >= 0; i--) {
+    for (let j = 0; j < activeAddress.length; j++) {
+      if (indexedAddress[i].toHexString() == activeAddress[j].toHexString()) {
+        indexedAddress.splice(i, 1)
+      }
+    }
+  }
   const activeValidators: string[] = []
   const indexedValidators: string[] = []
 
-  for (let i = BigInt.fromU32(0); i.lt(activeValidatorsLengthResult.value); i = i.plus(BigInt.fromU32(1))) {
-    activeValidators.push(createValidatorInfo(stakemanager.activeValidators(i).value0, block.number))
+  for (let i = 0; i < activeAddress.length; i++) {
+    activeValidators.push(createValidatorInfo(activeAddress[i], block.number, true))
   }
-  for (let i = BigInt.fromU32(0); i.lt(indexedValidatorsLength); i = i.plus(BigInt.fromU32(1))) {
-    indexedValidators.push(createValidatorInfo(stakemanager.indexedValidatorsByIndex(i), block.number))
+  for (let i = 0; i < indexedAddress.length; i++) {
+    indexedValidators.push(createValidatorInfo(indexedAddress[i], block.number, false))
   }
-  ValidatorInstance.activeValidator = activeValidators
-  ValidatorInstance.indexedValidator = indexedValidators
+  let validators = activeValidators.concat(indexedValidators)
+  ValidatorInstance.Validator = validators
   ValidatorInstance.save()
 }
