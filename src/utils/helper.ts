9c45685d16c3dc708f7801aeeacfe0352e674b48
sspin <826015751@qq.com>
2022-01-24 18:34:01 +0800

    chore: add validator active property

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -2,7 +2,7 @@ import { Getbalance } from '../types/DaliyData/Getbalance'
 import { Address } from '@graphprotocol/graph-ts'
 import { StakeManager } from '../types/StakeManager/StakeManager'
 
-const balanceAddress = '0xD4811fbC3FC5356F8A8e842410C88c0847EE8ED6'
+const balanceAddress = '0xE64Aa44254D9c468019d3Ef5273c1EE157ad28Ad'
 const stakeManagerAddress = '0x0000000000000000000000000000000000001001'
 export const feeAddress = '0x0000000000000000000000000000000000001005'
 export const getbalance = Getbalance.bind(Address.fromString(balanceAddress))
