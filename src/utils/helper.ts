0a2f489c09657a877fc7e8a525b4185464480cf4
sspin <826015751@qq.com>
2022-02-25 17:04:21 +0800

    add branch feat-mainnet

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -2,7 +2,7 @@ import { Getbalance } from '../types/DaliyData/Getbalance'
 import { Address } from '@graphprotocol/graph-ts'
 import { StakeManager } from '../types/StakeManager/StakeManager'
 
-const balanceAddress = '0xE64Aa44254D9c468019d3Ef5273c1EE157ad28Ad'
+const balanceAddress = '0x35656D8002448B68199d219c597eA35159B69099'
 const stakeManagerAddress = '0x0000000000000000000000000000000000001001'
 export const feeAddress = '0x0000000000000000000000000000000000001005'
 export const getbalance = Getbalance.bind(Address.fromString(balanceAddress))
