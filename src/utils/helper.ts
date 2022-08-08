138a09ffdfeb231184a60aeaaf34ded190f1934f
bijianing97 <826015751@qq.com>
2022-08-08 16:37:48 +0800

    chore: update balanceAddress

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -2,7 +2,7 @@ import { Getbalance } from '../types/DaliyData/Getbalance'
 import { Address } from '@graphprotocol/graph-ts'
 import { StakeManager } from '../types/StakeManager/StakeManager'
 
-const balanceAddress = '0xD1e57d532Ec2f4593266947574fCD55e56DEA99d'
+const balanceAddress = '0x35656D8002448B68199d219c597eA35159B69099'
 const stakeManagerAddress = '0x0000000000000000000000000000000000001001'
 export const feeAddress = '0x0000000000000000000000000000000000001005'
 export const getbalance = Getbalance.bind(Address.fromString(balanceAddress))
