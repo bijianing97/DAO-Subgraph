febf696fe51ef87ed8ea469544367b78d4ad98fa
bijianing97 <826015751@qq.com>
2022-12-12 16:17:10 +0800

    chore: add unjail logic

---
+++ src/Prison.ts
@@ -0,0 +1,58 @@
+import { Unjail } from './types/Prison/Prison'
+import { JailRecord, MinerInfo } from './types/schema'
+import { ethereum, BigInt } from '@graphprotocol/graph-ts'
+import { prison } from './utils/helper'
+
+export function handlePrisonBlock(block: ethereum.Block): void {
+  const jailedContracts = prison.getJailedMinersLength()
+  if (jailedContracts.gt(BigInt.fromI32(0))) {
+    for (let i = BigInt.fromI32(0); i.lt(jailedContracts); i = i.plus(BigInt.fromI32(1))) {
+      const minerAddress = prison.getJailedMinersByIndex(i)
+      let minerInfo = MinerInfo.load(minerAddress.toHex())
+      if (!minerInfo) {
+        minerInfo = new MinerInfo(minerAddress.toHex())
+        const jailRecord = new JailRecord(`${minerAddress.toHex()}-${block.number.toHex()}`)
+        jailRecord.address = minerAddress
+        jailRecord.timestamp = block.timestamp
+        jailRecord.blockNumber = block.number
+        jailRecord.save()
+        minerInfo.jailedRecord = [jailRecord.id]
+        minerInfo.lastJailedId = jailRecord.id
+        minerInfo.lastJailedNumber = block.number
+        minerInfo.jailed = true
+      } else if (!minerInfo.jailed) {
+        const jailRecord = new JailRecord(`${minerAddress.toHex()}-${block.number.toHex()}`)
+        jailRecord.address = minerAddress
+        jailRecord.timestamp = block.timestamp
+        jailRecord.blockNumber = block.number
+        jailRecord.save()
+        const jailedRecord = minerInfo.jailedRecord
+        jailedRecord.push(jailRecord.id)
+        minerInfo.jailedRecord = jailedRecord
+        minerInfo.lastJailedId = jailRecord.id
+        minerInfo.lastJailedNumber = block.number
+        minerInfo.jailed = true
+      } else {
+        continue
+      }
+      minerInfo.save()
+    }
+  }
+}
+
+export function handleUnjail(event: Unjail): void {
+  const minerAddress = event.params.miner
+  const minerInfo = MinerInfo.load(minerAddress.toHex())
+  if (minerInfo && minerInfo.jailed) {
+    const recordId = minerInfo.lastJailedId
+    const jailRecord = JailRecord.load(recordId)
+    if (jailRecord) {
+      minerInfo.jailed = false
+      jailRecord.unjailedTimestamp = event.block.timestamp
+      jailRecord.unjailedBlockNumber = event.block.number
+      jailRecord.unjailedForfeit = event.params.forfeit
+      jailRecord.save()
+    }
+    minerInfo.save()
+  }
+}
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,11 +1,22 @@
 import { Getbalance } from '../types/DaliyData/Getbalance'
 import { Address } from '@graphprotocol/graph-ts'
 import { StakeManager } from '../types/StakeManager/StakeManager'
+import { Prison } from '../types/Prison/Prison'
 
 const balanceAddress = '0x35656D8002448B68199d219c597eA35159B69099'
 const stakeManagerAddress = '0x0000000000000000000000000000000000001001'
+const prisonAddress = '0x0000000000000000000000000000000000001008'
 export const feeAddress = '0x0000000000000000000000000000000000001005'
 export const getbalance = Getbalance.bind(Address.fromString(balanceAddress))
 export const stakemanager = StakeManager.bind(Address.fromString(stakeManagerAddress))
+export const prison = Prison.bind(Address.fromString(prisonAddress))
 export const GasSaveInterval = 3600
 export const TotalStakeInterval = 3600
+export const hardforkBlock = 9629061
+export const genesisValidators = [
+  '0x2957879b3831b5ac1ef0ea1fb08dd21920f439b4',
+  '0xaa714ecc110735b4e114c8b35f035fc8706ff930',
+  '0x1b0885d33b43a696cd5517244a4fcb20b929f79d',
+  '0xb7a19f9b6269c26c5ef901bd128c364dd9ddc53a',
+  '0x0efe0da2b918412f1009337fe86321d88de091fb',
+]
