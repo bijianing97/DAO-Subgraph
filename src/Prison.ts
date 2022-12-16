05d662c48434bc5926e1702618ad5c56a515e16e
bijianing97 <826015751@qq.com>
2022-12-16 16:30:32 +0800

    chore: fix prison logic

--- src/Prison.ts
+++ src/Prison.ts
@@ -15,19 +15,7 @@ export function handlePrisonBlock(block: ethereum.Block): void {
   for (let i = BigInt.fromU32(0); i.lt(jailedContractsLength); i = i.plus(BigInt.fromU32(1))) {
     const minerAddress = prison.getJailedMinersByIndex(i)
     let minerInfo = MinerInfo.load(`${minerAddress.toHex()}`)
-    if (!minerInfo) {
-      minerInfo = new MinerInfo(`${minerAddress.toHex()}`)
-      const jailRecord = new JailRecord(`${minerAddress.toHex()}-${block.number.toString()}`)
-      jailRecord.address = minerAddress
-      jailRecord.timestamp = block.timestamp
-      jailRecord.blockNumber = block.number
-      jailRecord.save()
-      minerInfo.jailedRecord = [jailRecord.id]
-      minerInfo.lastJailedId = jailRecord.id
-      minerInfo.lastJailedNumber = block.number
-      minerInfo.jailed = true
-      minerInfo.save()
-    } else if (!minerInfo.jailed) {
+    if (minerInfo && minerInfo.jailed == false) {
       const jailRecord = new JailRecord(`${minerAddress.toHex()}-${block.number.toString()}`)
       jailRecord.address = minerAddress
       jailRecord.timestamp = block.timestamp
@@ -40,8 +28,18 @@ export function handlePrisonBlock(block: ethereum.Block): void {
       minerInfo.lastJailedNumber = block.number
       minerInfo.jailed = true
       minerInfo.save()
-    } else {
-      continue
+    } else if (!minerInfo) {
+      minerInfo = new MinerInfo(`${minerAddress.toHex()}`)
+      const jailRecord = new JailRecord(`${minerAddress.toHex()}-${block.number.toString()}`)
+      jailRecord.address = minerAddress
+      jailRecord.timestamp = block.timestamp
+      jailRecord.blockNumber = block.number
+      jailRecord.save()
+      minerInfo.jailedRecord = [jailRecord.id]
+      minerInfo.lastJailedId = jailRecord.id
+      minerInfo.lastJailedNumber = block.number
+      minerInfo.jailed = true
+      minerInfo.save()
     }
   }
 }
