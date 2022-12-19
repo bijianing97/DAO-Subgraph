b80c2f4809e6697d62e4a357743880cd41a86c2e
bijianing97 <826015751@qq.com>
2022-12-19 10:53:05 +0800

    chore: test validator

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,11 +1,11 @@
 import { Address } from '@graphprotocol/graph-ts'
-import { Prison } from '../types/Prison/Prison'
+import { StakeManager } from '../types/Validator/StakeManager'
 
 const balanceAddress = '0x56223c34367D366AA2572Cdaa63c88D059288506'
 const stakeManagerAddress = '0x0000000000000000000000000000000000001001'
 const prisonAddress = '0x0000000000000000000000000000000000001008'
 export const feeAddress = '0x0000000000000000000000000000000000001005'
-export const prison = Prison.bind(Address.fromString(prisonAddress))
+export const stakemanager = StakeManager.bind(Address.fromString(stakeManagerAddress))
 export const GasSaveInterval = 3600
 export const TotalStakeInterval = 3600
 export const hardforkBlock = 0
