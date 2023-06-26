a14ed3f79280a748ed144e66da46eb946e31b8bd
bijianing97 <826015751@qq.com>
2023-06-26 11:40:41 +0800

    chore: remove prison

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,13 +1,11 @@
 import { Address } from '@graphprotocol/graph-ts'
 import { StakeManager } from '../types/StakeManager/StakeManager'
-import { Prison } from '../types/Prison/Prison'
 
 const balanceAddress = '0x35656D8002448B68199d219c597eA35159B69099'
 const stakeManagerAddress = '0x0000000000000000000000000000000000001001'
 const prisonAddress = '0x0000000000000000000000000000000000001008'
 export const feeAddress = '0x0000000000000000000000000000000000001005'
 export const stakemanager = StakeManager.bind(Address.fromString(stakeManagerAddress))
-export const prison = Prison.bind(Address.fromString(prisonAddress))
 export const GasSaveInterval = 3600
 export const TotalStakeInterval = 3600
 export const hardforkBlock = 9629061
